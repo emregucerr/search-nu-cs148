1444c13573701baef37d5af6303161c399cffb54
megharamanathan <48444317+megharamanathan@users.noreply.github.com>
2020-09-23 17:54:11 -0500

    Initial commit

---
+++ student_code.py
@@ -0,0 +1,7 @@
+from expand import expand
+
+def a_star_search (dis_map, time_map, start, end):
+	path = []
+	# TODO Put your code here.
+	# be sure to call the imported function expand to get the next list of nodes
+	return path
