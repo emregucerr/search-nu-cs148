9b78b854afa2ed65bff9af82030a1b376c295108
Mohammed A. Alam <alam@Mohammeds-MacBook-Pro.local>
2021-01-25 19:13:52 -0600

    Added tests and a-star maps.

--- student_code.py
+++ student_code.py
@@ -1,7 +1,10 @@
 from expand import expand
 
 def a_star_search (dis_map, time_map, start, end):
-	path = []
-	# TODO Put your code here.
-	# be sure to call the imported function expand to get the next list of nodes
-	return path
+	pass
+
+def depth_first_search(time_map, start, end):
+	pass
+
+def breadth_first_search(time_map, start, end):
+	pass
